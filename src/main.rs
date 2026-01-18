67afeba8755c51351da72b1c1cabe048df4c9295
Bojan Čoka <bojan.coka@gmail.com>
2026-01-18 02:03:38 +0100

    Capture fleeting thoughts

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,24 @@
+use std::io::Write;
+
 fn main() {
-    println!("hello, world");
+    print!("What's on your mind?\n> ");
+    std::io::stdout().flush().unwrap();
+    let mut input = String::new();
+    std::io::stdin().read_line(&mut input).unwrap();
+    writeln!(
+        std::fs::OpenOptions::new()
+            .create(true)
+            .append(true)
+            .open(
+                std::path::PathBuf::from(std::env::var("HOME").unwrap())
+                    .join(".yaz")
+                    .join("notes.txt")
+            )
+            .unwrap(),
+        "[{}] {}",
+        chrono::Local::now().to_rfc2822(),
+        input.trim()
+    )
+    .unwrap();
+    println!("Thought persisted.");
 }
