17a6f4260de5f103a1452eba82dbf5dcafe5c5d9
Bojan Čoka <bojan.coka@gmail.com>
2026-02-01 18:49:14 +0100

    Ensure ~/.yaz/ exists before writing to it

--- src/main.rs
+++ src/main.rs
@@ -5,15 +5,13 @@ fn main() {
     std::io::stdout().flush().unwrap();
     let mut input = String::new();
     std::io::stdin().read_line(&mut input).unwrap();
+    let dir = std::path::PathBuf::from(std::env::var("HOME").unwrap()).join(".yaz");
+    std::fs::create_dir_all(&dir).unwrap();
     writeln!(
         std::fs::OpenOptions::new()
             .create(true)
             .append(true)
-            .open(
-                std::path::PathBuf::from(std::env::var("HOME").unwrap())
-                    .join(".yaz")
-                    .join("notes.txt")
-            )
+            .open(dir.join("notes.txt"))
             .unwrap(),
         "[{}] {}",
         chrono::Local::now().to_rfc2822(),
